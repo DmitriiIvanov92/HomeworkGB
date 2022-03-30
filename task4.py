9b80383fa2be80fab314920209a4a9391904d819
Dmitrii <deve11@yandex.ru>
2022-03-30 15:54:27 +0300

    Update task4.py 30.03.22

--- task4.py
+++ task4.py
@@ -14,10 +14,17 @@ result = [23, 1, 3, 10, 4, 11]
 
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 result_1 = [num for i, num in enumerate(src, 0) if num > src[i - 1] and i != 0]
-result_2 = (num for i, num in enumerate(src, 0) if num > src[i - 1] and i != 0)
 print(result_1)
-print(result_2)
+result_2 = (num for i, num in enumerate(src, 0) if num > src[i - 1] and i != 0)
 print(tuple(result_2))
+result_3 = [y for x, y in zip(src, src[1:]) if y > x]
+print(result_3)
+new_src = []
+for i in range(len(src) - 1):
+     if src[i] < src[i + 1]:
+        new_src.append(src[i + 1])
+print(new_src)
+
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 result = [i for i in src if src.count(i) == 1]
